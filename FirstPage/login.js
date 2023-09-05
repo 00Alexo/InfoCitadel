f4580a54b5445d677aad54494f3f60754cb59bb9
Alexo <alexsuciultc200@gmail.com>
2023-09-05 20:48:06 +0300

    more readable

--- FirstPage/login.js
+++ FirstPage/login.js
@@ -2,7 +2,11 @@ const SigninNavButton = document.querySelector('.SigninNavButton');
 let isDivVisual = false;
 
 SigninNavButton.addEventListener('click', (e) => {
-    divPrincipal.style.display == 'flex' ? divPrincipal.style.display = 'none': divPrincipal.style.display = 'flex'
+    if (divPrincipal.style.display === 'flex') {
+        divPrincipal.style.display = 'none';
+      } else {
+        divPrincipal.style.display = 'flex';
+      }
   });
   window.onkeydown = function(e) {
       if (e.keyCode === 27) {
