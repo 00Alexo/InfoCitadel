e2e57b09f391bb46de23e453f204600dbd358333
Alexo <alexsuciultc200@gmail.com>
2023-09-26 19:52:33 +0300

    dropDown finished

---
+++ FirstPage/navbar.js
@@ -0,0 +1,26 @@
+const secondForHoverDropdown = document.querySelector('.secondForHoverDropdown');
+const secondDropdownContent = document.querySelector('.secondDropdownContent');
+const dropDownContentJS = document.getElementById('dropDownContentJS');
+
+secondForHoverDropdown.addEventListener('click', () => {
+    const computedStyle = window.getComputedStyle(secondDropdownContent);
+    const visibility = computedStyle.getPropertyValue('visibility');
+
+    if (visibility === 'hidden' || visibility === '') {
+        secondDropdownContent.style.visibility = 'visible';
+        secondDropdownContent.style.opacity = '1';
+    } else {
+        secondDropdownContent.style.visibility = 'hidden';
+        secondDropdownContent.style.opacity = '1';
+    }
+});
+
+setInterval(() => {
+    const computedStyle = window.getComputedStyle(dropDownContentJS);
+    const visibility = computedStyle.getPropertyValue('visibility');
+
+    if (visibility === 'hidden' || visibility === '') {
+        secondDropdownContent.style.visibility = 'hidden';
+        secondDropdownContent.style.opacity = '0';
+    }
+}, 1);
