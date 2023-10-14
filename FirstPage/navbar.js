6dc63a8c2ed2fb5ca4c9b7e61c44542304d89c33
Alexo <alexsuciultc200@gmail.com>
2023-10-14 12:06:29 +0300

    da

--- FirstPage/navbar.js
+++ FirstPage/navbar.js
@@ -1,6 +1,9 @@
 const secondForHoverDropdown = document.querySelector('.secondForHoverDropdown');
 const secondDropdownContent = document.querySelector('.secondDropdownContent');
 const dropDownContentJS = document.getElementById('dropDownContentJS');
+const phone_secondForHoverDropdown = document.querySelector('.phone_secondForHoverDropdown');
+const phone_secondDropdownContent = document.querySelector('.phone_secondDropdownContent');
+const phone_dropDownContentJS = document.getElementById('phone_dropDownContentJS');
 const phone_NavBar = document.querySelector('.phone_NavBar');
 const phone_divSVG = document.querySelector('.phone_divSVG');
 const svgTreiBari = document.querySelector('.svgTreiBari');
@@ -37,5 +40,30 @@ setInterval(() => {
     if (visibility === 'hidden' || visibility === '') {
         secondDropdownContent.style.visibility = 'hidden';
         secondDropdownContent.style.opacity = '0';
+    }
+}, 1);
+
+
+
+phone_secondForHoverDropdown.addEventListener('click', () => {
+    const computedStyle = window.getComputedStyle(phone_secondDropdownContent);
+    const visibility = computedStyle.getPropertyValue('visibility');
+
+    if (visibility === 'hidden' || visibility === '') {
+        phone_secondDropdownContent.style.visibility = 'visible';
+        phone_secondDropdownContent.style.opacity = '1';
+    } else {
+        phone_secondDropdownContent.style.visibility = 'hidden';
+
+    }
+});
+
+setInterval(() => {
+    const computedStyle = window.getComputedStyle(phone_dropDownContentJS);
+    const visibility = computedStyle.getPropertyValue('visibility');
+
+    if (visibility === 'hidden' || visibility === '') {
+        phone_secondDropdownContent.style.visibility = 'hidden';
+        phone_secondDropdownContent.style.opacity = '0';
     }
 }, 1);
