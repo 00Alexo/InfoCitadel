98b24e0cf09e1d44741ffea098ce060f62813cc6
Alexo <alexsuciultc200@gmail.com>
2023-11-18 19:58:51 +0200

    da

--- FirstPage/carusel.js
+++ FirstPage/carusel.js
@@ -3,11 +3,26 @@ const changeColorButtons = document.querySelectorAll('.changeColorButton');
 const personDataPhoto = document.querySelector('.personDataPhoto');
 const motive = document.querySelector('.motive');
 const programmingLanguagesIcons = document.querySelector('.programmingLanguagesIcons');
+const photoPersonDataPhoto = document.querySelector('.photoPersonDataPhoto');
+const occupationPersonDataText = document.querySelector('.occupationPersonDataText');
+const namePersonDataText = document.querySelector('.namePersonDataText');
 
 changeColorButtons[0].style.backgroundColor = '#313131'; // Default color
 
 changeColorButtons[1].addEventListener('click', () =>{
+    mesaj.innerHTML = ' ';
+    stringAnimatedText = "Cand am descoperit acest site, m-am indoit de capacitatile mele ca web developer si mi-am dat seama ca dezvoltatorul este un geniu!!";
+    charAnimatedText = stringAnimatedText.split('');
+    mesaji=0;
     secondSignInView();
+});
+
+changeColorButtons[0].addEventListener('click', () =>{
+    mesaj.innerHTML = ' ';
+    let stringAnimatedText = "Site-ul acestui prieten este o sursă minunată. Plin de informații valoroase și ușor de navigat, te va captiva cu siguranță.";
+    charAnimatedText = stringAnimatedText.split('');
+    mesaji=0;
+    firstSignInView();
 });
 
 const secondSignInView = () => {
@@ -15,13 +30,28 @@ const secondSignInView = () => {
     moto.classList.add('motoSecondSignInView');
     motoContinuare.classList.add('motoContinuareSecondSignInView');
     comentariu.classList.add('comentariuSecondSignInView');
-    personDataText.classList.add('personDataTextSecondSignInView');
     personDataPhoto.classList.add('personDataPhotoSecondSignInView');
     carousel.classList.add('carouselSecondSignInView');
     programmingLanguagesIcons.classList.add('programmingLanguagesIconsSecondSignInView');
-    
+    photoPersonDataPhoto.src=('firstPage/Photos/veryadelin.png');
+    occupationPersonDataText.innerHTML = 'Student';
+    namePersonDataText.innerHTML = 'xndadelin';
+    animatedText();
 }
 
+const firstSignInView = () => {
+    motive.classList.remove('motiveSecondSignInView');
+    moto.classList.remove('motoSecondSignInView');
+    motoContinuare.classList.remove('motoContinuareSecondSignInView');
+    comentariu.classList.remove('comentariuSecondSignInView');
+    personDataPhoto.classList.remove('personDataPhotoSecondSignInView');
+    carousel.classList.remove('carouselSecondSignInView');
+    programmingLanguagesIcons.classList.remove('programmingLanguagesIconsSecondSignInView');
+    photoPersonDataPhoto.src=('firstPage/Photos/AlexoProfilePicture.png');
+    occupationPersonDataText.innerHTML = 'Web Developer';
+    namePersonDataText.innerHTML = 'SuperYang';
+    animatedText();
+}
 
 
 changeColorButtons.forEach((button) => {
--- FirstPage/effects.js
+++ FirstPage/effects.js
@@ -1,5 +1,5 @@
 //Astea s pentru animatia cu textul din comentarii
-const stringAnimatedText = "Site-ul acestui prieten este o sursă minunată. Plin de informații valoroase și ușor de navigat, te va captiva cu siguranță.";
+let stringAnimatedText = "Site-ul acestui prieten este o sursă minunată. Plin de informații valoroase și ușor de navigat, te va captiva cu siguranță.";
 let charAnimatedText = stringAnimatedText.split('');
 const mesaj = document.getElementById("mesaj");
 let mesaji=0;
@@ -8,7 +8,11 @@ const animatedText= () => {
 if (mesaji < charAnimatedText.length){
     mesaj.innerHTML += charAnimatedText[mesaji];
     mesaji++;
-    setTimeout(animatedText, 15);
+    if(stringAnimatedText.length > 125){
+    setTimeout(animatedText, 5);
+    }else{
+        setTimeout(animatedText, 12);
+    }
 }};
 
 
