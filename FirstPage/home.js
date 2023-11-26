e2ef77b70ba3a8e11885346ab907bca6267b8b05
Alexo <alexsuciultc200@gmail.com>
2023-11-26 13:49:44 +0200

    leaderboard style finished

--- FirstPage/home.js
+++ FirstPage/home.js
@@ -1,19 +1,46 @@
 // Leaderboard
 
-const jsShowOverflow = document.querySelector('.jsShowOverflow');
-const jsShowOverflow2 = document.querySelector('.jsShowOverflow2');
 const showMore = document.querySelector('.showMore');
+let showMoreToggle = false;
 const showMore2 = document.querySelector('.showMore2');
-
+let showMore2Toggle = false;
+const jsSimpleBarSelect = document.querySelectorAll('.jsSimpleBarSelect');
+const jsSimpleBarSelect2 = document.querySelectorAll('.jsSimpleBarSelect2');
+const simplebarContent = document.querySelector('.simplebar-content');
+const simplebarContent2 = document.querySelector('.simplebar-content-2');
 
 
 showMore.addEventListener('click', () => {
-    jsShowOverflow.classList.toggle('overflowhidden');
+    jsSimpleBarSelect2.forEach(ElmDiv => {
+        ElmDiv.classList.toggle('visually-hidden');
+    });
+    simplebarContent2.classList.toggle('simplebarReHeight');
+    console.log("simplebarContent2 height after toggle:", simplebarContent2.clientHeight);
+    if (showMoreToggle){
+        showMore.innerHTML="show more";
+        showMoreToggle = false;
+    }else{
+        showMore.innerHTML="show less";
+        showMoreToggle = true;
+    }
 });
 
 showMore2.addEventListener('click', () => {
-    jsShowOverflow2.classList.toggle('overflowhidden');
-});
+    jsSimpleBarSelect.forEach(ElmDiv => {
+        ElmDiv.classList.toggle('visually-hidden');
+    });
+    simplebarContent.classList.toggle('simplebarReHeight');
+    console.log("simplebarContent height after toggle:", simplebarContent.clientHeight);
+    if (showMore2Toggle){
+        showMore2.innerHTML="show more";
+        showMore2Toggle = false;
+    }else{
+        showMore2.innerHTML="show less";
+        showMore2Toggle = true;
+    }
+}); 
+
+
 
 
  
