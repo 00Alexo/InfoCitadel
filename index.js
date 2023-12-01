f8e6883ce502ec9674db903a6f243aa45eb77bf5
Alexo <alexsuciultc200@gmail.com>
2023-12-01 17:18:44 +0200

    da

--- index.js
+++ index.js
@@ -6,7 +6,28 @@ const hbs = require('hbs');
 const templatePath = path.join(__dirname);
 
 app.use(express.json());
-app.set('view engine', 'hbs');
+app.set('view engine', 'html');
+app.set('views', templatePath);
+app.use('/FirstPage', express.static(path.join(__dirname, 'FirstPage')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(templatePath, 'index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
+
+/*
+const express = require('express');
+const app = express();
+const path = require('path');
+const hbs = require('hbs');
+
+const templatePath = path.join(__dirname);
+
+app.use(express.json());
+app.set('view engine', 'html');
 app.set('views', templatePath);
 
 app.use('/FirstPage', express.static(path.join(__dirname, 'FirstPage')));
@@ -18,3 +39,4 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('listening on port 3000');
 });
+*/
