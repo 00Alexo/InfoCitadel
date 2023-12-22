c91aa3ec8a7ca26d0e7b21ad5f364a002a795b54
Alexo <alexsuciultc200@gmail.com>
2023-12-22 15:08:28 +0200

    testt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/carusel.css'
 import './css/login.css'
 import './css/navbar.css'
-import './css/home.css'
-import './css/register.css'
+// import './css/home.css'
+// import './css/register.css'
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
